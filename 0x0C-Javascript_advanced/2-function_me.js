64d2ccfd70e9cbc362aebe1e911b30b830ed1b94
jfangwang <qbs1864@gmail.com>
2021-09-20 12:17:13 -0400

    3 done

---
+++ 0x0C-Javascript_advanced/2-function_me.js
@@ -0,0 +1,8 @@
+// 2 = Function Me
+
+function welcomeMessage(fullName) {
+  alert("Welcome " + fullName);
+}
+var guillaume = welcomeMessage("Guillaume");
+var alex = welcomeMessage("Alex");
+var fred = welcomeMessage("Fred");
